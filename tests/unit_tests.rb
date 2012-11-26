82677ac95725cbc72a31cc8338ccc5c0aef57f5c
jamesshieh <randomletters13@gmail.com>
2012-11-26 15:19:52 -0500

    reworked interpreter for new parser

--- tests/unit_tests.rb
+++ tests/unit_tests.rb
@@ -4,27 +4,41 @@ require 'debugger'
 
 test0 = "A"
 parser = Parser.new(test0)
-parser.parse_trees
-interpreter = Interpreter.new(parser.parse_trees, parser.truth_table)
-print interpreter.validity?
+interpreter = Interpreter.new(parser.program, parser.truth_table)
+puts test0
+puts interpreter.truth_table
+print "Expecting: true, Got: "
+puts interpreter.validity?
 
 test1 = "A>C, A, !B, (A&!B)>D, A&(B|C)&D"
 parser = Parser.new(test1)
-interpreter = Interpreter.new(parser.parse_trees, parser.truth_table)
-print interpreter.validity?
+interpreter = Interpreter.new(parser.program, parser.truth_table)
+puts test1
+puts interpreter.truth_table
+print "Expecting: true, Got: "
+puts interpreter.validity?
 
 test2 = "(A&B)>C, A, !B, B|D, D>C"
 parser = Parser.new(test2)
-interpreter = Interpreter.new(parser.parse_trees, parser.truth_table)
-print interpreter.validity?
+interpreter = Interpreter.new(parser.program, parser.truth_table)
+puts test2
+puts interpreter.truth_table
+print "Expecting: true, Got: "
+puts interpreter.validity?
 
 test3 = "A|C, !C, B, (A&B)"
 parser = Parser.new(test3)
-interpreter = Interpreter.new(parser.parse_trees, parser.truth_table)
-print interpreter.validity?
+interpreter = Interpreter.new(parser.program, parser.truth_table)
+puts test3
+puts interpreter.truth_table
+print "Expecting: true, Got: "
+puts interpreter.validity?
 
 test4 = "(A&B)&(C&D)|E, A, B, !C, !D, E"
 parser = Parser.new(test4)
-interpreter = Interpreter.new(parser.parse_trees, parser.truth_table)
-print interpreter.validity?
+interpreter = Interpreter.new(parser.program, parser.truth_table)
+puts test4
+puts interpreter.truth_table
+print "Expecting: true, Got: "
+puts interpreter.validity?
 
